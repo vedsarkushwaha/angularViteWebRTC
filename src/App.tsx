5bc4bd2c9036fbe5bf2e33c0382dc9dcb4a4d0c5
Vedsar <vedsarkushwaha@gmail.com>
2024-11-03 21:11:38 -0800

    removing default code

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
   const [myUniqueId, setMyUniqueId] = useState<string>("");
 
   const generateRandomString= () => Math.random().toString(8).substring(2);
@@ -26,41 +24,17 @@ function App() {
         }
       });
     }
-
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
   }, []);
 
   useEffect(() => {
     setMyUniqueId(generateRandomString);
   }, [])
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
     <main>
       <div className='flex flex-col justify-center items-center p-12'>
         <p> your id: {myUniqueId} </p>
         <video className='w-72' playsInline ref={myVideoRef} autoPlay />
-      </div>
-
-
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
       </div>
     </main>
   );
